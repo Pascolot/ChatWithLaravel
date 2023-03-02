2755e0f3e7f20598b7ac0df74660ade5c465548e
JPrudence <rjeanprudence@gmail.com>
2023-03-02 13:29:22 +0300

    ajouter "en ligne" comme valeur par défaut du champ "statut"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('image', 400);
-            $table->string('status', 20);
+            $table->string('status', 20)->default('en ligne');
             $table->string('mdp');
             $table->rememberToken();
             $table->timestamps();
